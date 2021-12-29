b004675a13fcd722a1923c837d9f1b035f38c564
DiogoSilva11 <82061138+DiogoSilva11@users.noreply.github.com>
2021-12-29 20:54:31 +0000

    Position class added

--- src/main/java/Arena.java
+++ src/main/java/Arena.java
@@ -8,7 +8,7 @@ public class Arena {
     private final int width;
     private final int height;
 
-    public Arena(int width, int height){
+    public Arena(int width, int height) {
         this.width = width;
         this.height = height;
     }
