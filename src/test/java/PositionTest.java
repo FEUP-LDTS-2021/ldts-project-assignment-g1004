6eb04e770364d22a531b64ef686804c8c4f04369
DiogoSilva11 <82061138+DiogoSilva11@users.noreply.github.com>
2021-12-29 21:51:08 +0000

    Position tests

---
+++ src/test/java/PositionTest.java
@@ -0,0 +1,32 @@
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+
+public class PositionTest {
+    private Position position;
+
+    @BeforeEach
+    public void setup() {
+        position = new Position(1, 2);
+    }
+    /*
+    @Test
+    public void distanceTo() {
+        Position p2 = new Position(3, 5);
+
+        double d1 = position.distanceTo(p2);
+        double d2 = Math.sqrt(13);
+
+        Assertions.assertEquals(d2, d1);
+    }
+
+    @Test
+    public void moveTo() {
+        Position p2 = new Position(2, 4);
+        position.moveTo(p2);
+
+        Assertions.assertEquals(2, position.getX());
+        Assertions.assertEquals(4, position.getY());
+    }
+    */
+}
