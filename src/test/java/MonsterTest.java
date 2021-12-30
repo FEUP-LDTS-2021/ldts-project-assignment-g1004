abe94d69c86f2eca49263c5773258f218686c496
DiogoSilva11 <82061138+DiogoSilva11@users.noreply.github.com>
2021-12-30 20:00:56 +0000

    Monster test created

---
+++ src/test/java/MonsterTest.java
@@ -0,0 +1,29 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MonsterTest {
+    @Test
+    public void move() {
+        Platform platform = new Platform(new Position(1, 5), new Position(8, 5));
+        Goblin goblin = new Goblin(6, 4, platform);
+        Zombie zombie = new Zombie(3, 4, platform);
+        Ghost ghost = new Ghost(4, 4, platform);
+
+        goblin.move();
+        goblin.move();
+        goblin.move();
+        Assertions.assertFalse(goblin.movingForward());
+
+        zombie.move();
+        zombie.move();
+        zombie.move();
+        Position p = new Position(4, 4);
+        Assertions.assertEquals(p, zombie.getPosition());
+
+        ghost.move();
+        ghost.move();
+        ghost.move();
+        ghost.move();
+        Assertions.assertEquals(0, ghost.getSteps());
+    }
+}
