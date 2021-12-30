41c4428e24909a425d8642f7edc9992dd7fa35c5
DiogoSilva11 <82061138+DiogoSilva11@users.noreply.github.com>
2021-12-30 16:09:06 +0000

    Monster class removed (for now)

--- src/main/java/Monster.java
+++
@@ -1,9 +0,0 @@
-import com.googlecode.lanterna.graphics.TextGraphics;
-
-public abstract class Monster extends Element {
-    public Monster(int x, int y) {
-        super(x, y);
-    }
-
-    public abstract void draw(TextGraphics screen);
-}
