65235d64cec887063cc32bf569fa7032ebe9e5c2
DiogoSilva11 <82061138+DiogoSilva11@users.noreply.github.com>
2021-12-30 19:45:34 +0000

    Monster class updated

--- src/main/java/Monster.java
+++ src/main/java/Monster.java
@@ -1,9 +1,13 @@
 public abstract class Monster extends Element {
     protected Platform platform;
+    protected boolean forward;
+    protected int steps;
 
     public Monster(int x, int y, Platform p) {
         super(x, y);
         platform = p;
+        forward = true;
+        steps = 0;
     }
 
     public abstract Position move();
