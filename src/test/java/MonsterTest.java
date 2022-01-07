2e2ce20becaf4564a6cf40ddbeec7920a1e62bf2
Mariana Teixeira <76732179+marianalt@users.noreply.github.com>
2022-01-07 18:47:02 +0000

    Update MonsterTest.java

--- src/test/java/MonsterTest.java
+++ src/test/java/MonsterTest.java
@@ -1,18 +1,40 @@
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class MonsterTest {
+    private Platform platform;
+
+    @BeforeEach
+    public void setup() {
+        platform = new Platform(new Position(1, 5), new Position(8, 5));
+    }
+
     @Test
-    public void move() {
-        Platform platform = new Platform(new Position(1, 5), new Position(8, 5));
+    public void goblinTest() {
         Goblin goblin = new Goblin(6, 4, platform);
-        Zombie zombie = new Zombie(3, 4, platform);
-        Ghost ghost = new Ghost(4, 4, platform);
+
+        MoveStrategy g = goblin.getMovement();
+        Assertions.assertTrue(g instanceof RegularStrategy);
+        goblin.checkDirection();
+        Assertions.assertTrue(goblin.movingForward());
 
         goblin.move();
         goblin.move();
         goblin.move();
         Assertions.assertFalse(goblin.movingForward());
+    }
+
+    @Test
+    public void zombieTest() {
+        Zombie zombie = new Zombie(3, 4, platform);
+
+        MoveStrategy z = zombie.getMovement();
+        Assertions.assertTrue(z instanceof ConfusedStrategy);
+        zombie.step();
+        Assertions.assertEquals(1, zombie.getSteps());
+        zombie.reset();
+        Assertions.assertEquals(0, zombie.getSteps());
 
         zombie.move();
         zombie.move();
@@ -20,6 +42,14 @@ public class MonsterTest {
         Position p = new Position(4, 4);
         Assertions.assertEquals(p, zombie.getPosition());
         Assertions.assertTrue(zombie.movingForward());
+    }
+
+    @Test
+    public void ghostTest() {
+        Ghost ghost = new Ghost(4, 4, platform);
+
+        MoveStrategy h = ghost.getMovement();
+        Assertions.assertTrue(h instanceof TeleportationStrategy);
 
         ghost.move();
         ghost.move();
