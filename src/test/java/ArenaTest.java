97e3c7d27153293eba984e7945ffeb21e528452b
DiogoSilva11 <82061138+DiogoSilva11@users.noreply.github.com>
2022-01-08 10:43:51 +0000

    Small change in diagram

--- src/test/java/ArenaTest.java
+++ src/test/java/ArenaTest.java
@@ -16,6 +16,34 @@ import java.io.IOException;
 
 public class ArenaTest {
     @Test
+    public void moveRight() {
+        KeyStroke key = Mockito.mock(KeyStroke.class);
+        Mockito.when(key.getKeyType()).thenReturn(KeyType.ArrowRight);
+
+        Arena arena = new Arena(); // hero spawns at coordinates (1, 18)
+        arena.processKey(key);
+
+        Assertions.assertEquals(2, arena.getHero().getPosition().getX());
+    }
+
+    @Test
+    public void buryHero() {
+        Position position = Mockito.mock(Position.class);
+        Mockito.when(position.getX()).thenReturn(1);
+        Mockito.when(position.getY()).thenReturn(19);
+
+        Arena arena = new Arena(); // hero spawns at coordinates (1, 18)
+
+        arena.processKey(new KeyStroke(KeyType.ArrowRight)); // just to change hero's X value
+        arena.processKey(new KeyStroke(KeyType.ArrowDown)); // just to set current direction as 'v'
+
+        arena.moveHero(position);
+
+        Assertions.assertEquals(2, arena.getHero().getPosition().getX());
+        Assertions.assertEquals(18, arena.getHero().getPosition().getY());
+    }
+
+    @Test
     public void heroCall() {
         Arena arena = Mockito.spy(Arena.class);
         // hero starts at position (1, 18)
