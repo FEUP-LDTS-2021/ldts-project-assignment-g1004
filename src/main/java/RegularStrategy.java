b6421e2ccfde9f5260f647175e47d6f2f25be6eb
malu1608 <mlflsalvador@gmail.com>
2022-01-09 10:59:16 +0000

    little change (regular strategy)

--- src/main/java/RegularStrategy.java
+++ src/main/java/RegularStrategy.java
@@ -1,5 +1,5 @@
 /**
- * Strategy in which monster moves to the left border and then to the right border of the platform.
+ * Regular Strategy class. Each monster moves to the left border and then to the right border of the platform.
  */
 public class RegularStrategy implements MoveStrategy {
     /**
