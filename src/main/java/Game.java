8fe5f2cacf4cae33a36eb23c839dff47e184716d
Mariana Teixeira <76732179+marianalt@users.noreply.github.com>
2022-01-09 10:32:45 +0000

    Docs for game class

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -8,10 +8,17 @@ import com.googlecode.lanterna.terminal.Terminal;
 
 import java.io.IOException;
 
+/**
+ * Game class. This class sets the screen and the arena or level.
+ */
 public class Game {
-    private Screen screen;
-    private Arena arena;
+    private Screen screen;  /** screen where game will be shown */
+    private Arena arena;    /** the arena with game components */
 
+    /**
+     * Constructor.
+     * It initializes arena and implements Lanterna methods, adding the screen to the arena.
+     */
     public Game() {
         try {
             TerminalSize terminalSize = new TerminalSize(60, 20);
@@ -29,16 +36,29 @@ public class Game {
         arena = new Arena();
     }
 
+    /**
+     * Draws the executable screen,
+     * @throws IOException
+     */
     private void draw() throws IOException {
         screen.clear();
         arena.draw(screen.newTextGraphics());
         screen.refresh();
     }
 
+    /**
+     * When user input is obtained (key), the game handles a response from the game.
+     * @param key is the user input.
+     */
     private void processKey(KeyStroke key) {
         arena.processKey(key);
     }
 
+    /**
+     * This method shows the main loop of the game.
+     * When we run the game, the terminal is drawn and user input is processed.
+     * It also checks if hero collided with monsters or, after moving them, if any monster collided with hero.
+     */
     public void run() {
         try {
             while (true) {
