9625d663c2e2a2ef95e55538ce9641a1ba2cc3c8
malu1608 <mlflsalvador@gmail.com>
2022-01-09 11:21:07 +0000

    Docs for goblin class

--- src/main/java/Goblin.java
+++ src/main/java/Goblin.java
@@ -3,16 +3,34 @@ import com.googlecode.lanterna.TerminalPosition;
 import com.googlecode.lanterna.TextColor;
 import com.googlecode.lanterna.graphics.TextGraphics;
 
+/**
+ * Goblin monster class.
+ * This monster adopts a strategy in which he moves forward to a corner of the platform and then reverses its direction, moving to the other corner.
+ */
 public class Goblin extends Monster {
+    /**
+     * Constructor.
+     * @param x coordinate.
+     * @param y coordinate.
+     * @param p platform.
+     */
     public Goblin(int x, int y, Platform p) {
         super(x, y, p);
     }
 
+    /**
+     * Defines strategy for goblin moves, according to its special features.
+     * @return goblin strategy.
+     */
     @Override
     protected MoveStrategy createMoveStrategy() {
         return new RegularStrategy();
     }
 
+    /**
+     * Draw goblin on the screen.
+     * @param screen
+     */
     @Override
     public void draw(TextGraphics screen) {
         screen.setForegroundColor(TextColor.Factory.fromString("#00FF00"));
@@ -20,6 +38,11 @@ public class Goblin extends Monster {
         screen.putString(new TerminalPosition(getPosition().getX(), getPosition().getY()), "g");
     }
 
+    /**
+     * Comparison between position of the goblin and position of an object.
+     * @param o
+     * @return true if the position of both objects is the same.
+     */
     @Override
     public boolean equals(Object o) {
         if (o == null || this.getClass() != o.getClass())
