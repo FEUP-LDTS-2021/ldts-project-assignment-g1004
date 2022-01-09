534e9129228f07b85b85d724635bd9ab965ecc65
Mariana Teixeira <76732179+marianalt@users.noreply.github.com>
2022-01-09 10:41:57 +0000

    Doc for platform class

--- src/main/java/Platform.java
+++ src/main/java/Platform.java
@@ -3,17 +3,29 @@ import com.googlecode.lanterna.graphics.TextGraphics;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Platform class. This class creates and draws the platforms where the hero can move horizontally.
+ */
 public class Platform {
-    private final Position left;
-    private final Position right;
-    private final List<Wall> walls;
+    private final Position left;        /** left corner of the platform */
+    private final Position right;       /** right corner of the platform */
+    private final List<Wall> walls;     /** list of walls that compose the platform */
 
+    /**
+     * Constructor. A platform is composed by many wall units.
+     * @param left is the position where the left corner of the platform is.
+     * @param right is the position where the right corner of the platform is.
+     */
     public Platform(Position left, Position right) {
         this.left = left;
         this.right = right;
         walls = createWalls();
     }
 
+    /**
+     * Creates wall units that will be used to form a platform.
+     * @return wall units that will compose a platform.
+     */
     private List<Wall> createWalls() {
         List<Wall> walls = new ArrayList<>();
 
@@ -24,18 +36,34 @@ public class Platform {
         return walls;
     }
 
+    /**
+     * Obtain left corner of the platform.
+     * @return left edge.
+     */
     public Position getLeft() {
         return left;
     }
 
+    /**
+     * Obtain right corner on the platform.
+     * @return right edge.
+     */
     public Position getRight() {
         return right;
     }
 
+    /**
+     * Obtain the list of walls that forms the platforms.
+     * @return list of platforms.
+     */
     public List<Wall> getWalls() {
         return walls;
     }
 
+    /**
+     * Draws a platform on the screen.
+     * @param screen
+     */
     public void draw(TextGraphics screen) {
         for (Wall w : walls)
             w.draw(screen);
