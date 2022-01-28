5fa6f1a56cbc22b19bdd755093373590d03282c6
DiogoSilva11 <82061138+DiogoSilva11@users.noreply.github.com>
2022-01-28 01:03:37 +0000

    Documentation inside position class removed (NO CODE CHANGES!)

--- src/main/java/com/g1004/getout/position/Position.java
+++ src/main/java/com/g1004/getout/position/Position.java
@@ -1,51 +1,26 @@
 package com.g1004.getout.position;
 
-/**
- * com.g1004.getout.position.Position class. Describes location of elements.
- */
 public class Position {
+    private int x;
+    private int y;
 
-    private int x; /** Abscissa */
-    private int y; /** Ordinate */
-
-    /**
-     * Default constructor.
-     */
     public Position() {
         this(0, 0);
     }
 
-    /**
-     * Constructor.
-     * @param x coordinate.
-     * @param y coordinate.
-     */
     public Position(int x, int y) {
         this.x = x;
         this.y = y;
     }
 
-    /**
-     * Obtain x coordinate.
-     * @return abscissa.
-     */
     public int getX() {
         return x;
     }
 
-    /**
-     * Obtain y coordinate.
-     * @return ordinate.
-     */
     public int getY() {
         return y;
     }
 
-    /**
-     * Comparison between positions of two objects, by comparing its coordinates.
-     * @param o
-     * @return true if the position of both objects is the same.
-     */
     @Override
     public boolean equals(Object o) {
         if (this == o)
@@ -56,19 +31,10 @@ public class Position {
         return x == position.x && y == position.y;
     }
 
-    /**
-     * Calculates the distance between positions.
-     * @param position
-     * @return returns the distance.
-     */
     public double distanceTo(Position position) {
         return Math.sqrt(Math.pow(this.x - position.x, 2) + Math.pow(this.y - position.y, 2));
     }
 
-    /**
-     * Changes to another position.
-     * @param position
-     */
     public void moveTo(Position position) {
         this.x = position.getX();
         this.y = position.getY();
